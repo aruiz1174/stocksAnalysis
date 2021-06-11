d119f7473ce38e8d701ee6a58a8565a6b92676a0
truizbon <truizbonls>
2021-06-10 20:09:53 -0400

    fixing formatting

---
+++ cmake-build-debug/CMakeFiles/stocksAnalysis.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tony/stocksAnalysis/Portfolio.cpp" "/Users/tony/stocksAnalysis/cmake-build-debug/CMakeFiles/stocksAnalysis.dir/Portfolio.cpp.o"
+  "/Users/tony/stocksAnalysis/Stock.cpp" "/Users/tony/stocksAnalysis/cmake-build-debug/CMakeFiles/stocksAnalysis.dir/Stock.cpp.o"
+  "/Users/tony/stocksAnalysis/SymbolPair.cpp" "/Users/tony/stocksAnalysis/cmake-build-debug/CMakeFiles/stocksAnalysis.dir/SymbolPair.cpp.o"
+  "/Users/tony/stocksAnalysis/SymbolTable.cpp" "/Users/tony/stocksAnalysis/cmake-build-debug/CMakeFiles/stocksAnalysis.dir/SymbolTable.cpp.o"
+  "/Users/tony/stocksAnalysis/main.cpp" "/Users/tony/stocksAnalysis/cmake-build-debug/CMakeFiles/stocksAnalysis.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
